691fd27d67b3126afaa8873063432922896138da
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-08-13 11:20:54 -0300

    separando widgets da tela de cadastro

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/register_text_field.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
 
@@ -8,10 +10,11 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
-  String nome = '';
+  String name = '';
+  String password = '';
+  String confirmPassword = '';
   String email = '';
-  String senha = '';
-  String usuario = '';
+  String user = '';
 
   @override
   Widget build(BuildContext context) {
@@ -30,69 +33,74 @@ class _RegisterViewState extends State<RegisterView> {
                     alignment: Alignment.center,
                     child: Image.asset('assets/Logo.png'),
                   ),
-                  buildTextField(
-                      onChanged: (text) {
-                        nome = text;
-                      },
-                      icon: Icons.perm_identity_sharp,
-                      fillColor: const Color(0xFFA4D8E5),
-                      iconColor: Colors.white,
-                      labelText: 'Nome',
-                      labelColor: Colors.white),
-                  SizedBox(height: 10),
-                  buildTextField(
-                      onChanged: (text) {
-                        email = text;
-                      },
-                      icon: Icons.email_outlined,
-                      fillColor: const Color(0xFFA4D8E5),
-                      iconColor: Colors.white,
-                      labelText: 'E-mail',
-                      labelColor: Colors.white),
-                  SizedBox(height: 10),
-                  buildTextField(
-                      onChanged: (text) {
-                        usuario = text;
-                      },
-                      icon: Icons.perm_identity_sharp,
-                      fillColor: const Color(0xFFA4D8E5),
-                      iconColor: Colors.white,
-                      labelText: 'Usuário',
-                      labelColor: Colors.white),
-                  SizedBox(height: 10),
-                  buildTextField(
-                      onChanged: (text) {
-                        senha = text;
+                  RegisterTextField(
+                    onChange: (text) {
+                      name = text;
+                    },
+                    icon: Icons.perm_identity_sharp,
+                    fillColor: const Color(0xFFA4D8E5),
+                    iconColor: Colors.white,
+                    labelText: 'Nome',
+                    labelColor: Colors.white,
+                  ),
+                  const SizedBox(height: 10),
+                  RegisterTextField(
+                    onChange: (text) {
+                      email = text;
+                    },
+                    icon: Icons.email_outlined,
+                    fillColor: const Color(0xFFA4D8E5),
+                    iconColor: Colors.white,
+                    labelText: 'E-mail',
+                    labelColor: Colors.white,
+                  ),
+                  const SizedBox(height: 10),
+                  RegisterTextField(
+                    onChange: (text) {
+                      user = text;
+                    },
+                    icon: Icons.perm_identity_sharp,
+                    fillColor: const Color(0xFFA4D8E5),
+                    iconColor: Colors.white,
+                    labelText: 'Usuário',
+                    labelColor: Colors.white,
+                  ),
+                  const SizedBox(height: 10),
+                  RegisterTextField(
+                    onChange: (text) {
+                      password = text;
+                    },
+                    icon: Icons.lock_outline,
+                    fillColor: const Color(0xFFA4D8E5),
+                    iconColor: Colors.white,
+                    labelText: 'Senha',
+                    obscureText: true,
+                    labelColor: Colors.white,
+                  ),
+                  const SizedBox(height: 10),
+                  RegisterTextField(
+                      onChange: (text) {
+                        confirmPassword = text;
                       },
                       icon: Icons.lock_outline,
                       fillColor: const Color(0xFFA4D8E5),
                       iconColor: Colors.white,
-                      labelText: 'Senha',
                       obscureText: true,
-                      labelColor: Colors.white),
-                  SizedBox(height: 10),
-                  buildTextField(
-                      onChanged: (text) {
-                        senha = text;
-                      },
-                      icon: Icons.lock_outline,
-                      fillColor: const Color(0xFFA4D8E5),
-                      iconColor: Colors.white,
                       labelText: 'Confirmar senha',
                       labelColor: Colors.white),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   Container(
                     height: 45,
                     child: TextButton(
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF0292B7),
+                        backgroundColor: const Color(0xFF0292B7),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(50),
                         ),
                         padding: const EdgeInsets.symmetric(horizontal: 110.0),
                       ),
                       onPressed: () {},
-                      child: Text(
+                      child: const Text(
                         'Confirmar',
                         style: TextStyle(
                             fontSize: 20,
@@ -101,45 +109,11 @@ class _RegisterViewState extends State<RegisterView> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 40),
+                  const SizedBox(height: 40),
                 ],
               ),
             )
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildTextField(
-      {required ValueChanged<String> onChanged,
-      required String labelText,
-      required labelColor,
-      required Color fillColor,
-      required Color iconColor,
-      required IconData icon,
-      bool obscureText = false}) {
-    return Container(
-      height: 45,
-      padding: EdgeInsets.symmetric(horizontal: 50),
-      child: TextField(
-        obscureText: obscureText,
-        onChanged: onChanged,
-        style: TextStyle(color: Colors.white, fontSize: 15),
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: TextStyle(color: labelColor),
-          filled: true,
-          fillColor: fillColor,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(50.0),
-            borderSide: BorderSide.none,
-          ),
-          prefixIcon: Padding(
-            child: Icon(icon, color: iconColor),
-            padding: EdgeInsets.all(5),
-          ),
         ),
       ),
     );
